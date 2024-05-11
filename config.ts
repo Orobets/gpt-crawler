df8762a74b5fde381ea2b8bbdff1534f50b2ff39
it2b <131894745+Orobets@users.noreply.github.com>
2024-05-11 14:07:15 +0300

    kendra

--- config.ts
+++ config.ts
@@ -1,12 +1,12 @@
 import { Config } from "./src/config";
 
-const baseUrl = "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest";
-const prefix = "";
+const baseUrl = "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/";
+const prefix = "client/kendra-ranking/";
 
 export const defaultConfig: Config = {
   url: `${baseUrl}${prefix}`,
   match: `${baseUrl}${prefix}**`,
   maxPagesToCrawl: 10000,
-  outputFileName: "aws.json",
+  outputFileName: prefix.split("/").reverse().join("-") + ".json",
   maxTokens: 2000000,
 };
