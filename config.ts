be8614acb65d0ba8572ce32d82b0e8d0949c82c1
it2b <131894745+Orobets@users.noreply.github.com>
2024-05-11 11:57:31 +0300

    prefix

--- config.ts
+++ config.ts
@@ -1,9 +1,12 @@
 import { Config } from "./src/config";
 
+const baseUrl = "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest";
+const prefix = "";
+
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: `${baseUrl}${prefix}`,
+  match: `${baseUrl}${prefix}**`,
+  maxPagesToCrawl: 10000,
+  outputFileName: "aws.json",
   maxTokens: 2000000,
 };
